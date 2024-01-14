55f49819eee3ef221ac54e46d94fd0cebf4bc2db
ShikshaFinder <janiharsh794@gmail.com>
2024-01-14 12:49:34 +0530

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Flex,
+
+  Button,
+   useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import Hero from "../../src/components/hero"
+interface Props {
+  children: React.ReactNode;
+}
+
+const NavLink = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={"#"}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+    const color = colorMode === "dark" ? "white" : "black";
+
+  return (
+    
+    <>
+
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Box color={color}>Artificial Intelligence Firm</Box>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+
+
+      </Box>
+<Hero/>
+      
+
+
+
+    </>
+  );
+}
